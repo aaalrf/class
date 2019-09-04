f6a6fc6521b096b9079804ffd3f9cc29c62f4d8c
aaalrf <aaa88815115@outlook.com>
2019-09-04 16:13:39 +0800

    2019.9.4 16:13

---
+++ test/src/JPanel_Exam.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class JPanel_Exam {
+    public static void main(String[] args){
+        JFrame mainJFrame=new JFrame("框架+面板示例");
+        mainJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainJFrame.setSize(240,240);
+        mainJFrame.setLocation(200,200);
+        Container container=new Container();
+        container=mainJFrame.getContentPane();
+        container.setBackground(Color.lightGray);
+        container.setLayout(null);
+        JPanel pn1=new JPanel();
+        pn1.setSize(190,180);
+        pn1.setBackground(Color.green);
+        pn1.setLocation(10,50);
+        JButton btn=new JButton("我是一个按钮，单击我");
+        btn.setSize(170,40);
+        btn.setBackground(Color.yellow);
+        btn.setLocation(10,50);
+        pn1.setLayout(null);
+        pn1.add(btn);
+        container.add(pn1);
+        mainJFrame.setVisible(true);
+    }
+}
